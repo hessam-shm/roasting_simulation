9edc9e2ae729d0a28c4e98bc09b8955bc1d1a281
hessam-shm <hessam@ymail.com>
2021-04-02 00:18:26 +0430

    dockerized local uses default postgres installation, with docker a separate machine comes up for database

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM maven:3-alpine AS build
+ENV TZ Europe/Vienna
+WORKDIR /app
+COPY ./pom.xml ./pom.xml
+RUN mvn dependency:go-offline -B
+COPY ./src ./src
+RUN mvn -f ./pom.xml clean package
+
+FROM openjdk:8-jdk-alpine
+ENV TZ Europe/Vienna
+WORKDIR /app
+COPY --from=build /app/target/roasting-simulation-0.0.1-SNAPSHOT.jar /app/application.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "/app/application.jar"]
--- src/main/java/com/cropster/roastingsimulation/RoastingSimulationApplication.java
+++ src/main/java/com/cropster/roastingsimulation/RoastingSimulationApplication.java
@@ -2,8 +2,9 @@ package com.cropster.roastingsimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class RoastingSimulationApplication {
 
     public static void main(String[] args) {
